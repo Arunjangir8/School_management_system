ba1ab74759426eb39b9fe702f4d6630f86a57638
Arunjangir8 <arunjangir9987@example.com>
2025-06-15 00:31:18 +0530

    Added : announcements events and result with database

--- src/app/(dashboard)/list/announcements/page.tsx
+++ src/app/(dashboard)/list/announcements/page.tsx
@@ -3,15 +3,13 @@ import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
 import { announcementsData, role } from "@/lib/data";
+import prisma from "@/lib/prisma";
+import { ITEM_PER_PAGE } from "@/lib/settings";
+import { Announcement, Class, Prisma } from "@prisma/client";
 import Image from "next/image";
 import Link from "next/link";
 
-type Announcement = {
-    id: number;
-    title: string;
-    class: string;
-    date: string;
-};
+
 
 const columns = [
     {
@@ -27,33 +25,69 @@ const columns = [
         accessor: "date",
         className: "hidden md:table-cell",
     },
-    {
-        header: "Actions",
-        accessor: "action",
-    },
-];
+    ...(role === "admin"
+      ? [
+          {
+            header: "Actions",
+            accessor: "action",
+          },
+        ]
+      : []),
+  ];
+type AnnouncementList = Announcement & { class: Class };
+const renderRow = (item: AnnouncementList) => (
+    <tr
+        key={item.id}
+        className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight"
+    >
+        <td className="flex items-center gap-4 p-4">{item.title}</td>
+        <td>{item.class?.name || "-"}</td>
+        <td className="hidden md:table-cell">{new Intl.DateTimeFormat("en-US").format(item.date)}</td>
+        <td>
+            <div className="flex items-center gap-2">
+                {role === "admin" && (
+                    <>
+                        <FormModel table="announcement" type="update" data={item} />
+                        <FormModel table="announcement" type="delete" id={item.id} />
+                    </>
+                )}
+            </div>
+        </td>
+    </tr>
+);
 
-const AnnouncementListPage = () => {
-    const renderRow = (item: Announcement) => (
-        <tr
-            key={item.id}
-            className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight"
-        >
-            <td className="flex items-center gap-4 p-4">{item.title}</td>
-            <td>{item.class}</td>
-            <td className="hidden md:table-cell">{item.date}</td>
-            <td>
-                <div className="flex items-center gap-2">
-                    {role === "admin" && (
-            <>
-              <FormModel table="announcement" type="update" data={item} />
-              <FormModel table="announcement" type="delete" id={item.id} />
-            </>
-          )}
-                </div>
-            </td>
-        </tr>
-    );
+const AnnouncementListPage = async ({ searchParams, }: { searchParams: { [key: string]: string | undefined } }) => {
+    const { page, ...queryParams } = searchParams;
+    const p = page ? parseInt(page) : 1;
+
+    const query: Prisma.AnnouncementWhereInput = {};
+
+    if (queryParams) {
+        for (const [key, value] of Object.entries(queryParams)) {
+            if (value !== undefined) {
+                switch (key) {
+                    case "search":
+                        query.title = { contains: value, mode: "insensitive" };
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+
+    const [data, count] = await prisma.$transaction([
+        prisma.announcement.findMany({
+            where: query,
+            include: {
+                class : true
+            },
+            take: ITEM_PER_PAGE,
+            skip: ITEM_PER_PAGE * (p - 1),
+        }),
+        prisma.announcement.count({ where: query }),
+    ]);
+
 
     return (
         <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
@@ -70,12 +104,12 @@ const AnnouncementListPage = () => {
                         <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
                             <Image src="/sort.png" alt="" width={14} height={14} />
                         </button>
-                        {role === "admin" &&  <FormModel table="announcement" type="create" />}
+                        {role === "admin" && <FormModel table="announcement" type="create" />}
                     </div>
                 </div>
             </div>
-            <Table columns={columns} renderRow={renderRow} data={announcementsData} />
-            <Pagination />
+            <Table columns={columns} renderRow={renderRow} data={data} />
+            <Pagination page={p} count={count} />
         </div>
     );
 };
