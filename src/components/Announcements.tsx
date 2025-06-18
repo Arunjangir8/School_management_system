81f84ae66ee3f0707fd91c096191ebf2371bf024
Arunjangir8 <arunjangir9987@example.com>
2025-06-18 22:39:01 +0530

    Admin Page Done

--- src/components/Announcements.tsx
+++ src/components/Announcements.tsx
@@ -1,25 +1,25 @@
-const announcements = [
-  {
-    id: 1,
-    time: '2025-06-08',
-    title: 'New Feature Released',
-    description: 'We’ve rolled out a new dashboard view to help you track your performance more effectively.',
-  },
-  {
-    id: 2,
-    time: '2025-06-05',
-    title: 'Scheduled Maintenance',
-    description: 'Our platform will undergo maintenance on June 12 from 12:00 AM to 2:00 AM UTC. Services may be temporarily unavailable.',
-  },
-  {
-    id: 3,
-    time: '2025-06-01',
-    title: 'Welcome New Users!',
-    description: 'We’re excited to welcome over 1,000 new users to our platform this month. Thank you for joining!',
-  },
-];
+import prisma from "@/lib/prisma";
+import { currentUser } from "@clerk/nextjs/server";
 
-const Announcements = () => {
+const Announcements = async () => {
+  const user =  await currentUser();
+  const role = (user?.publicMetadata as { role?: string })?.role;
+  const id = user?.id;
+  const roleBased ={
+    admin : {},
+    teacher : {lessons : {some : {teacherId : id!}}},
+    student :{students : {some : {id : id!}}},
+    parent : {students : {some : {parentId : id!}}},
+  }
+  const data = await prisma.announcement.findMany({
+    take :3,
+    orderBy : {date : 'desc'},
+    where :{
+      ...(role !=="admin" && {OR :[
+        {classId : null} ,{class : roleBased[role as keyof typeof roleBased] || {}}
+      ]})
+    }
+  })
     return (
         <div className='bg-white p-4 rounded-md'>
             <div className="flex justify-between items-center">
@@ -27,11 +27,16 @@ const Announcements = () => {
                 <span className="text-xs text-gray-400">View all</span>
             </div>
             <div className="flex flex-col gap-4 mt-4">
-                {announcements.map((ann)=>(
+                {data.map((ann)=>(
                 <div className="odd:bg-LamaSkyLight even:bg-LamaPurpleLight p-4 rounded-md" key={ann.id}>
                     <div className="flex justify-between items-center">
                         <h2 className="font-medium">{ann.title}</h2>
-                        <span className="text-xs rounded-md bg-white text-gray-400 px-1 py-1">{ann.time}</span>
+                        <span className="text-xs rounded-md bg-white text-gray-400 px-1 py-1"> {new Date(ann.date).toLocaleTimeString('en-IN', {
+                        timeZone: 'Asia/Kolkata',
+                        hour: '2-digit',
+                        minute: '2-digit',
+                        hour12: true,
+                    })}</span>
                     </div>
                     <p className="text-xs text-gray-400 mt-1">{ann.description }</p>
                 </div>
